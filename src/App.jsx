8f8579b4c5bc527d5303ac0fd8d045a94b23bd64
knight-Security <paliwaladitya24@gmail.com>
2026-05-29 01:15:48 +0530

    Added pin array feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
 import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
 const App = () => {
+  const [pinArray, setPinArray] = useState([])
+  function saveCity(city) {
+    setPinArray((prev) => [...prev, city])
+  }
   return (
     <div className='parent'>
       <Sidebar />
       <div className="main">
-        <Outlet />
+        <Outlet context={{ pinArray, saveCity }} />
       </div>
     </div>
   )
