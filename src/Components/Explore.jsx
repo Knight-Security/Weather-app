f5a4d0e153afd2e7f804d538b64690ee7f9f416d
knight-Security <paliwaladitya24@gmail.com>
2026-05-29 01:17:20 +0530

    Added pin array feature

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -1,34 +1,50 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import Result from './Result'
+import { useOutletContext } from 'react-router-dom'
 const Explore = () => {
   const [recievedData, setrecievedData] = useState(null)
+  const { saveCity } = useOutletContext()
   const [pinArray, setpinArray] = useState([])
   const [city, setcity] = useState("")
   const OPEN_WEATHER_API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY
   async function fetchData() {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
-      try {
-        const response = await fetch(url)
-        const data = await response.json()
-        setrecievedData(data)
-        console.log(data)
-      }
-      catch (err) {
-        console.log(err.message)
-      }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setrecievedData(data)
     }
+    catch (err) {
+      console.log(err.message)
+    }
+  }
   return (
     <div>
       <h1>DISCOVER</h1>
       <h1>THE ATMOSPHERE</h1>
       <div className="search-bar">
         <div className="Search-button-parent">
-          < input value={city} onChange={(e) => setcity(e.target.value)} type="text" className='search-input' placeholder='Searching.....' />
-          <button className="serach-button" onClick={() => fetchData()}><span className="material-symbols-outlined">
-            search
-          </span></button>
-          {recievedData && <Result data={recievedData}/>}
+          <div className="Search-button-child">
+            < input value={city} onChange={(e) => setcity(e.target.value)} onKeyDown={
+              (e) => {
+                if (e.key == "Enter") {
+                  fetchData()
+                }
+              }
+            } type="text" className='search-input' placeholder='Searching.....' />
+            <button className="serach-button" onClick={() => fetchData()}><span className="material-symbols-outlined">
+              search
+            </span></button>
+          </div>
+          {recievedData &&
+
+            <Result
+              data={recievedData}
+              saveCity={saveCity}
+            />
+
+          }
         </div>
         <div className="trendingnow-bar">
           <h2>Trending Now</h2>
